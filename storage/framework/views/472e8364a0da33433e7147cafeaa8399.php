07a88911ed860e5577a6bea114ee72bc9ae8b47f
Damian Navarro <78166533+Naronitsu@users.noreply.github.com>
2025-03-29 09:06:55 +0100

    Delete storage directory

--- storage/framework/views/472e8364a0da33433e7147cafeaa8399.php
+++
@@ -1,9 +0,0 @@
-<form method="GET" action="<?php echo e(route('students.index')); ?>" class="me-2">
-    <div class="d-flex align-items-center">
-        <label for="sort" class="me-2">Sort by</label>
-        <select name="sort" class="form-select" onchange="this.form.submit()">
-            <option value="name" <?php echo e(request('sort') == 'name' ? 'selected' : ''); ?>>Name</option>
-        </select>
-    </div>
-</form>
-<?php /**PATH /home/sss/myCollegeManager/resources/views/students/_sort.blade.php ENDPATH**/ ?>
